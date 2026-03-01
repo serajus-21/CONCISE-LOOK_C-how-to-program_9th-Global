f651be8f0925d87f6304c582129e7655dc95e161
serajus-21 <serajussalehin21@gmail.com>
2026-03-01 11:10:43 +0600

    prac

---
+++ chapter-5/math-funcs.c
@@ -0,0 +1,11 @@
+//math-funcs.c
+
+#include<stdio.h>
+#include<math.h>
+
+int main(void) {
+    // double sqroot = sqrt(900);
+    double sinValue = sin(90);
+
+    printf("%.2f\n", sinValue);
+}
---
+++ run.sh
@@ -0,0 +1,12 @@
+for((chapter=1; chapter>=1 && chapter<=4; chapter++))
+do
+    # mkdir chapter-$chapter
+    rmdir chapter-$chapter
+done
+
+# for (( chapter=1; chapter<=3; chapter++ ))
+# do
+#     # mkdir chapter-$chapter
+#     rmdir chapter-$chapter
+# done
+
